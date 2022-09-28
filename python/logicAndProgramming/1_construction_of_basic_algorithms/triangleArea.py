ddae7ed1e6de6d93f043214bee0054983add233e
juliancastanedaDev <juliandavidcastaneda@gmail.com>
2022-09-28 18:42:31 -0500

    first exercise, area of ​​a triangle

---
+++ python/logicAndProgramming/1_construction_of_basic_algorithms/triangleArea.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""
+    his script calculates the area of ​​a triangle
+    b = Base of the triangle
+    h = height of the triangle
+"""
+b = int(input("Por favor ingrese la base del triangulo "))
+h = int(input("Por favor ingrese la altura del triangulo "))
+
+area = (b * h) / 2
+print("El area del triangulo es {}".format(area))
